87629873e20100f216fb2ac8577be1eefbf811a5
gabbrieu <gabrielhmendes@hotmail.com>
2021-08-27 20:09:49 -0300

    refactor: inital configs and changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended',
+		'prettier',
+	],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		'comma-dange': ['error', 'always-multiline'],
+		'import/first': 'off',
+		'import/order': 'off',
+		ident: 'off',
+		'@typescript-eslint/ident': ['error', 'tab'],
+		'no-new': 'off',
+		'no-tabs': 'off',
+		semi: ['error', 'always'],
+		'vue/html-ident': ['error', 'tab'],
+		'vue/order-in-components': 'off',
+	},
 }
