09dd0f3bf5bfb0983ec3945ae51fffb1b7b4a4da
gabbrieu <gabrielhmendes@hotmail.com>
2021-09-15 00:15:29 -0300

    feat: countdown logic added

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,7 @@
 			flex flex-col
 			lg:flex-row
 			flex-1
-			lg:flex-none
-			lg:mt-16
+			lg:flex-none lg:mt-16
 			sm:gap-x-10
 			md:gap-x-20
 		"
@@ -13,12 +12,30 @@
 		<div class="flex flex-col w-full lg:w-1/2">
 			<Profile />
 			<CompletedChallenges />
+			<Countdown @completed="getNewChallenge" />
+			<!-- ou v-on:completed= -->
+			<button v-if="hasCountdownCompleted" disabled class="button completed">
+				Cycle completed
+			</button>
+			<button
+				v-else-if="isCountdownActive"
+				class="button abandon"
+				@click="setCountdownState(false)"
+			>
+				Abandon cycle
+			</button>
+			<button v-else class="button start" @click="setCountdownState(true)">
+				Start a cycle
+			</button>
 		</div>
 	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapState, mapMutations } from 'vuex';
+import { Mutations as CountdownMT } from '~/store/Countdown/types';
+import { playAudio, sendNotification } from '~/utils';
 
 interface Head {
 	title: string;
@@ -29,6 +46,38 @@ export default Vue.extend({
 		return {
 			title: 'Home | movue.it',
 		};
+	},
+	computed: {
+		...mapState('Countdown', {
+			hasCountdownCompleted: 'hasCompleted',
+			isCountdownActive: 'isActive',
+		}),
+	},
+	// Função executada após o componente ser montado na tela
+	mounted() {
+		if ('Notification' in window) {
+			Notification.requestPermission();
+		}
+	},
+	methods: {
+		...mapMutations({
+			setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
+		}),
+		setCountdownState(flag: boolean) {
+			this.setCountdownHasCompleted(false);
+			this.setCountdownIsActive(flag);
+		},
+		getNewChallenge() {
+			this.setCountdownHasCompleted(true);
+			if (Notification?.permission === 'granted') {
+				playAudio('/notification.mp3');
+				sendNotification('New Challenge', {
+					body: 'A new challenge has started! Go complete it!',
+					icon: '/favicon.png',
+				});
+			}
+		},
 	},
 });
 </script>
